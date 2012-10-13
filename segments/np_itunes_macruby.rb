53a6d176e6ed036e80bfd59254bd348a784ef503
Kjell <kjell@leanside.com>
2012-10-12 21:32:09 -0500

    Re-enable my np_itunes_macruby

--- segments/np_itunes_macruby.rb
+++ segments/np_itunes_macruby.rb
@@ -1,9 +1,9 @@
 #!/usr/local/bin/macruby
 
-# framework 'ScriptingBridge'
-# itunes = SBApplication.applicationWithBundleIdentifier("com.apple.itunes")
+framework 'ScriptingBridge'
+itunes = SBApplication.applicationWithBundleIdentifier("com.apple.itunes")
 
-# if itunes.playerState == 1800426320
-#   playing = "♫ " + (itunes.currentStreamTitle || "#{itunes.currentTrack.artist} - #{itunes.currentTrack.name}")
-#   puts playing.length > 50 ? playing[0..50] + "…" : playing
-# end
+if itunes.playerState == 1800426320
+  playing = "♫ " + (itunes.currentStreamTitle || "#{itunes.currentTrack.artist} - #{itunes.currentTrack.name}")
+  puts playing.length > 50 ? playing[0..50] + "…" : playing
+end
